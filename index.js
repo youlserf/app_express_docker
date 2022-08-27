7bd8026b1a28d06c007d5a4efd4c6307b7e0d386
Youlserf <youlserf.cardenas@tecsup.edu.pe>
2022-08-27 12:21:59 -0500

    proposed exercises completed

--- index.js
+++ index.js
@@ -1,49 +1,72 @@
-import express, { request, response } from "express";
+import express from "express";
 
 const app = express();
 
 app.use(express.json()); //definir el metodo en el que se resivira la data
 
-const people = [];
+const people = [
+    {
+        id:  1,
+        name: "pepe",
+        las_name: "perez",
+        age: 25,
+    },
+    {
+        id:  2,
+        name: "pepe1",
+        las_name: "perez1",
+        age: 251,
+    }
+];
 
-
+//http://localhost:6004/
 app.get("/", (request, response)=>{
     return response.json({
         ok: true,
-        data: 
-        //array
-        [
-            //objeto
-            {
-                id:  1,
-                name: "pepe",
-                las_name: "perez",
-                age: 25,
-            },
-            {
-                id:  1,
-                name: "pepe1",
-                las_name: "perez1",
-                age: 251,
-            }
-            
-        ],
+        data: people,
     });
 });
 
-app.listen(6004, ()=> 
-    console.log(`El servidor inicio  en http://localhost:6004`)
-    );
+//http://localhost:6004/read/1
+app.get("/read/:id", (request, response)=>{
+    const id = parseInt(request.params.id);
+    let n;
 
+    people.forEach((element, index) => {   
+        if(element.id === id){
+            n = index;
+        } 
+    });
+
+    if(n >= 0){
+        return response.status(201).json(
+            {
+                ok: true,
+                data: people[n]
+            }
+        );
+    }else{
+        return response.status(404).json(
+            {
+                ok: false,
+                data: people[n]
+            }
+        ); 
+    }
+    
+});
+
+http://localhost:6004/create
 app.post("/create", (request, response)=>{
     //destructurar un obtjeto {name, email}
    /*  const {name, last_name} = request.body;  */
-    people.push(request.body); 
+    const data = request.body;
+    data.id = people.length + 1;
+    people.push(data); 
 
     //404 cuando no se encuentra el archivo
     //201 creacion exitosa
     //500 error en el servidor
-    
 
     return response.status(201).json(
         {
@@ -52,3 +75,67 @@ app.post("/create", (request, response)=>{
         }
     );
 })
+
+http://localhost:6004/update
+app.put("/update", (request, response)=>{
+    const data = request.body;
+    let n;
+
+    people.forEach((element, index) => {   
+        if(element.id === data.id){
+            data.id = people[index].id;
+            people[index] = data;
+            n = index;
+        } 
+    });
+
+    if(n >= 0){
+        return response.status(201).json(
+            {
+                ok: true,
+                data: people[n]
+            }
+        );
+    }else{
+        return response.status(404).json(
+            {
+                ok: false,
+                data: people[n]
+            }
+        );
+    }
+    
+    
+})
+
+http://localhost:6004/delete/1
+app.delete("/delete/:id", (request, response)=>{
+    const id = parseInt(request.params.id);
+    let removedEl;
+    people.forEach((element, index) => {
+        if(element.id === id) {
+            removedEl = people.splice(index, index + 1);
+        }
+    });
+
+    if(removedEl){
+        return response.status(201).json(
+            {
+                ok: true,
+                data: removedEl
+            }
+        );
+    }else{
+        return response.status(404).json(
+            {
+                ok: false,
+                data: removedEl
+            }
+        );     
+    }   
+})
+
+
+app.listen(6004, ()=> 
+    console.log(`El servidor inicio  en http://localhost:6004`)
+);
